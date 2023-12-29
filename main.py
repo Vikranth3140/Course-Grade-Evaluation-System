ab2cba6a999669f5fc2ea76f771dbe2a96664ef7
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-12-30 03:25:51 +0530

    Added the terminal functionality to make it clean

--- main.py
+++ main.py
@@ -1,89 +1,92 @@
 import sys
 import logging
 
-def setup_logging():
-    logging.basicConfig(filename='grade_calculator.log', level=logging.INFO)
-
-class Student:
-    def __init__(self, student_id, marks):
-        self.student_id = student_id
-        self.marks = marks
-
-class GradeCalculator:
-    def __init__(self, weights):
+class GradeProcessor:
+    def __init__(self, input_file_path, output_file_path, weights):
+        self.input_file_path = input_file_path
+        self.output_file_path = output_file_path
         self.weights = weights
+        self.students = []
 
-    def calculate_weighted_sum(self, student):
-        weighted_sum = sum(mark * weight[1] / weight[0] for mark, weight in zip(student.marks, self.weights))
-        return weighted_sum
+    def setup_logging(self):
+        logging.basicConfig(filename='grade_calculator.log', level=logging.INFO)
 
-    def calculate_grade(self, total_marks):
-        if total_marks > 80:
-            return 'A'
-        elif 80 >= total_marks > 70:
-            return 'A-'
-        elif 70 >= total_marks > 60:
-            return 'B'
-        elif 60 >= total_marks > 50:
-            return 'B-'
-        elif 50 >= total_marks > 40:
-            return 'C'
-        elif 40 >= total_marks > 35:
-            return 'C-'
-        elif 35 >= total_marks > 30:
-            return 'D'
-        else:
-            return 'F'
+    class Student:
+        def __init__(self, student_id, marks):
+            self.student_id = student_id
+            self.marks = marks
 
-def read_student_data(file_path):
-    students = []
-    try:
-        with open(file_path, 'r') as file:
-            for line in file:
-                data = line.strip().split(',')
-                student_id = int(data[0])
-                marks = list(map(int, data[1:]))
-                students.append(Student(student_id, marks))
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-    except Exception as e:
-        print(f"An unexpected error occurred while reading data: {e}")
-    return students
+    class GradeCalculator:
+        def __init__(self, weights):
+            self.weights = weights
 
-def write_grade_data(students, grade_calculator, output_file_path):
-    try:
-        with open(output_file_path, 'w') as file:
-            file.write("Student ID\t\tTotal Marks\t\tGrade\n")
-            for student in students:
-                weighted_sum = grade_calculator.calculate_weighted_sum(student)
-                total_marks = round(weighted_sum, 2)
-                grade = grade_calculator.calculate_grade(total_marks)
-                file.write(f"{student.student_id}\t\t\t\t{total_marks}\t\t\t{grade}\n")
-    except Exception as e:
-        print(f"An unexpected error occurred while writing data: {e}")
+        def calculate_weighted_sum(self, student):
+            weighted_sum = sum(mark * weight[1] / weight[0] for mark, weight in zip(student.marks, self.weights))
+            return weighted_sum
 
-def main():
-    setup_logging()
-    try:
-        if len(sys.argv) != 3:
-            print("Usage: python main.py input_file output_file")
-            return
+        def calculate_grade(self, total_marks):
+            if total_marks > 80:
+                return 'A'
+            elif 80 >= total_marks > 70:
+                return 'A-'
+            elif 70 >= total_marks > 60:
+                return 'B'
+            elif 60 >= total_marks > 50:
+                return 'B-'
+            elif 50 >= total_marks > 40:
+                return 'C'
+            elif 40 >= total_marks > 35:
+                return 'C-'
+            elif 35 >= total_marks > 30:
+                return 'D'
+            else:
+                return 'F'
 
-        input_file_path = sys.argv[1]
-        output_file_path = sys.argv[2]
+    def read_student_data(self):
+        try:
+            with open(self.input_file_path, 'r') as file:
+                for line in file:
+                    data = line.strip().split(',')
+                    student_id = int(data[0])
+                    marks = list(map(int, data[1:]))
+                    self.students.append(self.Student(student_id, marks))
+        except FileNotFoundError:
+            print(f"Error: File '{self.input_file_path}' not found.")
+        except Exception as e:
+            print(f"An unexpected error occurred while reading data: {e}")
 
-        weights = [(10, 5), (20, 5), (100, 15), (40, 10), (100, 35), (100, 30)]
-        students = read_student_data(input_file_path)
-        
-        if students:
-            logging.info("Student data loaded successfully.")
-            grade_calculator = GradeCalculator(weights)
-            write_grade_data(students, grade_calculator, output_file_path)
-            logging.info(f"Grade data written successfully to '{output_file_path}'.")
-        else:
-            logging.warning("No student data found.")
-    except Exception as e:
-        logging.error(f"An unexpected error occurred: {e}")
+    def write_grade_data(self):
+        try:
+            with open(self.output_file_path, 'w') as file:
+                file.write("Student ID\t\tTotal Marks\t\tGrade\n")
+                for student in self.students:
+                    weighted_sum = self.grade_calculator.calculate_weighted_sum(student)
+                    total_marks = round(weighted_sum, 2)
+                    grade = self.grade_calculator.calculate_grade(total_marks)
+                    file.write(f"{student.student_id}\t\t\t\t{total_marks}\t\t\t{grade}\n")
+        except Exception as e:
+            print(f"An unexpected error occurred while writing data: {e}")
+
+    def process_grades(self):
+        self.setup_logging()
+        try:
+            if len(sys.argv) != 3:
+                print("Usage: python main.py input_file output_file")
+                return
+
+            self.grade_calculator = self.GradeCalculator(self.weights)
+            self.read_student_data()
+
+            if self.students:
+                logging.info("Student data loaded successfully.")
+                self.write_grade_data()
+                logging.info(f"Grade data written successfully to '{self.output_file_path}'.")
+            else:
+                logging.warning("No student data found.")
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+    weights = [(10, 5), (20, 5), (100, 15), (40, 10), (100, 35), (100, 30)]
+    processor = GradeProcessor(sys.argv[1], sys.argv[2], weights)
+    processor.process_grades()
