7e753c888c5989964db69e66752ae86795380c4f
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-12-30 03:22:14 +0530

    Added logging

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import logging
+
+def setup_logging():
+    logging.basicConfig(filename='grade_calculator.log', level=logging.INFO)
+
 class Student:
     def __init__(self, student_id, marks):
         self.student_id = student_id
@@ -47,28 +53,37 @@ def read_student_data(file_path):
 def write_grade_data(students, grade_calculator, output_file_path):
     try:
         with open(output_file_path, 'w') as file:
-            file.write("Student ID, Total Marks, Grade\n\n")
+            file.write("Student ID\t\tTotal Marks\t\tGrade\n")
             for student in students:
                 weighted_sum = grade_calculator.calculate_weighted_sum(student)
                 total_marks = round(weighted_sum, 2)
                 grade = grade_calculator.calculate_grade(total_marks)
-                file.write(f"{student.student_id}, {total_marks}, {grade}\n")
+                file.write(f"{student.student_id}\t\t\t\t{total_marks}\t\t\t{grade}\n")
     except Exception as e:
         print(f"An unexpected error occurred while writing data: {e}")
 
 def main():
+    setup_logging()
     try:
+        if len(sys.argv) != 3:
+            print("Usage: python main.py input_file output_file")
+            return
+
+        input_file_path = sys.argv[1]
+        output_file_path = sys.argv[2]
+
         weights = [(10, 5), (20, 5), (100, 15), (40, 10), (100, 35), (100, 30)]
-        students = read_student_data("IPmarks.txt")
+        students = read_student_data(input_file_path)
+        
         if students:
-            print("Student data loaded successfully.")
+            logging.info("Student data loaded successfully.")
             grade_calculator = GradeCalculator(weights)
-            write_grade_data(students, grade_calculator, "IPgrade.txt")
-            print("Grade data written successfully to 'IPgrade.txt'.")
+            write_grade_data(students, grade_calculator, output_file_path)
+            logging.info(f"Grade data written successfully to '{output_file_path}'.")
         else:
-            print("No student data found.")
+            logging.warning("No student data found.")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        logging.error(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     main()
