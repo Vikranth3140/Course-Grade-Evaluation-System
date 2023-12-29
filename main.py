90c806864d01a4835b130cb5fe878b3d25077ecb
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-12-30 03:21:55 +0530

    Added tabular ui for showing grades in file

--- main.py
+++ main.py
@@ -41,17 +41,18 @@ def read_student_data(file_path):
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(f"An unexpected error occurred while reading data: {e}")
     return students
 
 def write_grade_data(students, grade_calculator, output_file_path):
     try:
         with open(output_file_path, 'w') as file:
+            file.write("Student ID, Total Marks, Grade\n\n")
             for student in students:
                 weighted_sum = grade_calculator.calculate_weighted_sum(student)
                 total_marks = round(weighted_sum, 2)
                 grade = grade_calculator.calculate_grade(total_marks)
-                file.write(f"{student.student_id},{total_marks},{grade}\n")
+                file.write(f"{student.student_id}, {total_marks}, {grade}\n")
     except Exception as e:
         print(f"An unexpected error occurred while writing data: {e}")
 
@@ -59,8 +60,13 @@ def main():
     try:
         weights = [(10, 5), (20, 5), (100, 15), (40, 10), (100, 35), (100, 30)]
         students = read_student_data("IPmarks.txt")
-        grade_calculator = GradeCalculator(weights)
-        write_grade_data(students, grade_calculator, "IPgrade.txt")
+        if students:
+            print("Student data loaded successfully.")
+            grade_calculator = GradeCalculator(weights)
+            write_grade_data(students, grade_calculator, "IPgrade.txt")
+            print("Grade data written successfully to 'IPgrade.txt'.")
+        else:
+            print("No student data found.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
