e01b56a98f816a440c0a4db7e0cb4b02cbf31341
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-12-30 05:37:32 +0530

    Fixed the writing to screen erros

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ class Student:
 
     def calculate_grade_counts(self):
         logging.info("Calculating grade counts.")
+
         grade_counts = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'F': 0}
         for grade in self.grades.values():
             grade_counts[grade] += 1
@@ -114,29 +115,49 @@ class Student:
         with open(file_path, 'w') as f:
             f.write(tabulate(data, headers=headers, tablefmt="grid"))
 
+        print()
         print(f"Course Summary written to '{file_path}'.")
-        logging.info(f"Course Summary written to '{file_path}'.")
 
     def show_grades(self):
         logging.info("Showing grades.")
-        print("Students Grade Summary shown in 'Student's Grade Summary' text file.")
-        with open("Student's Grade Summary", "w") as f:
-            f.write("Student ID\t\t\tTotal Marks\t\t\tGrade\n\n")
-            for roll_no, grade in self.grades.items():
-                f.write(f"{roll_no}\t\t\t\t{self.percentages[roll_no]}\t\t\t\t{grade}\n")
+        file_path = "Students' Grade Summary.txt"
+
+        with open(file_path, "w") as f:
+            data = [(roll_no, self.percentages[roll_no], grade) for roll_no, grade in self.grades.items()]
+            f.write(tabulate(data, headers=["Student ID", "Total Marks", "Grade"], tablefmt="grid"))
+
+        print()
+        print(f"Students Grade Summary written to '{file_path}'.")
 
     def search_student_record(self, roll_no):
         logging.info(f"Searching for student with Roll No.: {roll_no}")
         if roll_no in self.calculator.students_data:
-            print(f'The Marks: {self.calculator.students_data[roll_no]}')
-            print(f'The Grade: {self.grades[roll_no]}')
-            print(f'The Percentage: {self.percentages[roll_no]}')
+            
+            # Get component names and weights
+            component_names = [weight[0] for weight in self.calculator.weights]
+            component_weights = [f"{weight[1]}%" for weight in self.calculator.weights]
+
+            # Display individual marks using tabulate
+            individual_marks = [["Subject", "Marks"]]
+            individual_marks.extend(list(zip(component_names, self.calculator.students_data[roll_no])))
+            print(tabulate(individual_marks, headers="firstrow", tablefmt="grid"))
+
+            # Write details to "Student's Grade Record" file
+            with open("Student's Grade Record", "w") as f:
+                f.write(f"Student ID: {roll_no}\n\n")
+                f.write(tabulate(individual_marks, headers="firstrow", tablefmt="grid"))
+                f.write(f'\n\nThe Grade: {self.grades[roll_no]}\n')
+                f.write(f'The Percentage: {self.percentages[roll_no]}\n')
         else:
             print('Student with that Roll No not found')
             logging.warning(f"Student with Roll No. {roll_no} not found.")
 
+
 if __name__ == "__main__":
     # Input from user
+    print()
+    print('Welcome to Grade Evaluation System')
+    print()
     course_name = input('Enter Course Name: ')
     credits = int(input('Enter Credits: '))
     file_path = input('Enter file path: ')
@@ -151,15 +172,20 @@ if __name__ == "__main__":
         print("1. Generate summary of the course")
         print("2. Show the grades of all students")
         print("3. Search student's record")
+        print()
         choice = input('Enter your choice (press Enter to exit): ')
         if choice == '1':
+            print()
             student_instance.display_course_summary()
         elif choice == '2':
+            print()
             student_instance.show_grades()
         elif choice == '3':
+            print()
             roll_no = int(input("Enter Roll No.: "))
             student_instance.search_student_record(roll_no)
         elif not choice:
+            print()
             print('Thank You!!!')
             break
         else:
