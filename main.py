19db0b56ef86a93bef4945352869cc850fbcb7f3
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-12-30 05:54:45 +0530

    Fixed the logging error

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ logging.basicConfig(filename='grad_eval.log', level=logging.DEBUG, format='%(asc
 
 class GradeCalculator:
     def __init__(self, file_path):
+        # Make your changes here
         self.file_path = file_path
-        self.weights = [("labs", 30), ("midsem", 15), ("assignments", 30), ("endsem", 25)]
+        self.weights = [("Labs", 30), ("Midsem", 15), ("Assignments", 30), ("Endsem", 25)]
         self.policy = [80, 65, 50, 40]
         self.students_data = {}
         self.percentages = {}
 
     def read_students_data(self):
-        logging.info(f"Reading data from file: {self.file_path}")
+        logging.info("Reading students' data.")
         try:
             with open(self.file_path, 'r') as file:
                 for line in file:
@@ -21,6 +22,7 @@ class GradeCalculator:
                     roll_no = int(data[0])
                     marks = list(map(int, data[1:]))
                     self.students_data[roll_no] = marks
+                    logging.info(f"Succesfully read data for Roll No. {roll_no}.")
         except FileNotFoundError as e:
             logging.error(f"Error: File '{self.file_path}' not found.")
             raise e
@@ -35,14 +37,17 @@ class GradeCalculator:
             weighted_sum = sum(mark * weight[1] / 100 for mark, weight in zip(marks, self.weights))
             percentages[roll_no] = round(weighted_sum, 2)
         self.percentages = percentages
+        logging.info("Percentages calculated.")
         return percentages
 
     def calculate_grades(self, percentages):
         logging.info("Calculating grades.")
+
         grades = {}
         for roll_no, percentage in percentages.items():
             grade = self.get_grade(percentage)
             grades[roll_no] = grade
+        logging.info("Grades calculated.")
         return grades
 
     def get_grade(self, percentage):
@@ -59,6 +64,7 @@ class GradeCalculator:
 
     def update_policy(self):
         logging.info("Updating policy.")
+
         for i in range(len(self.policy)):
             temp_list = [j for j in self.percentages.values() if abs(self.policy[i] - j) <= 2]
             if temp_list:
@@ -67,6 +73,11 @@ class GradeCalculator:
                 if diff:
                     self.policy[i] = round((temp_list[diff.index(max(diff))] + temp_list[diff.index(max(diff)) + 1]),
                                            2) / 2
+                else:
+                    self.policy[i] = temp_list[0]
+            else:
+                self.policy[i] = self.policy[i]
+        logging.info("Policy updated.")
 
 class Student:
     def __init__(self, calculator):
@@ -77,11 +88,13 @@ class Student:
 
     def calculate_statistics(self):
         logging.info("Calculating statistics.")
+
         self.calculator.read_students_data()
         self.percentages = self.calculator.calculate_percentages()
         self.calculator.update_policy()
         self.grades = self.calculator.calculate_grades(self.percentages)
         self.calculate_grade_counts()
+        logging.info("Statistics calculated.")
 
     def calculate_grade_counts(self):
         logging.info("Calculating grade counts.")
@@ -90,9 +103,9 @@ class Student:
         for grade in self.grades.values():
             grade_counts[grade] += 1
         self.dict_count = grade_counts
+        logging.info("Grade counts calculated.")
 
     def display_course_summary(self):
-        logging.info("Displaying course summary.")
         file_path = "Course Summary"
 
         headers = ["Course", "Credits", "Component", "Weightage", "Cutoffs", "Grading Summary"]
@@ -117,9 +130,10 @@ class Student:
 
         print()
         print(f"Course Summary written to '{file_path}'.")
+        print()
+        logging.info(f"Course Summary written to '{file_path}'.")
 
     def show_grades(self):
-        logging.info("Showing grades.")
         file_path = "Students' Grade Summary.txt"
 
         with open(file_path, "w") as f:
@@ -127,7 +141,9 @@ class Student:
             f.write(tabulate(data, headers=["Student ID", "Total Marks", "Grade"], tablefmt="grid"))
 
         print()
-        print(f"Students Grade Summary written to '{file_path}'.")
+        print(f"Students' Grade Summary written to '{file_path}'.")
+        print()
+        logging.info(f"Students' Grade Summary written to '{file_path}'.")
 
     def search_student_record(self, roll_no):
         logging.info(f"Searching for student with Roll No.: {roll_no}")
@@ -140,14 +156,17 @@ class Student:
             # Display individual marks using tabulate
             individual_marks = [["Subject", "Marks"]]
             individual_marks.extend(list(zip(component_names, self.calculator.students_data[roll_no])))
-            print(tabulate(individual_marks, headers="firstrow", tablefmt="grid"))
 
             # Write details to "Student's Grade Record" file
-            with open("Student's Grade Record", "w") as f:
+            with open("Student's Grade Record.txt", "w") as f:
                 f.write(f"Student ID: {roll_no}\n\n")
                 f.write(tabulate(individual_marks, headers="firstrow", tablefmt="grid"))
                 f.write(f'\n\nThe Grade: {self.grades[roll_no]}\n')
                 f.write(f'The Percentage: {self.percentages[roll_no]}\n')
+                logging.info(f"Student with Roll No. {roll_no} found.")
+                print()
+                print(f"Student's Grade Record written to '{f}'.")
+                print()
         else:
             print('Student with that Roll No not found')
             logging.warning(f"Student with Roll No. {roll_no} not found.")
@@ -169,6 +188,7 @@ if __name__ == "__main__":
     # Perform calculations and display options
     student_instance.calculate_statistics()
     while True:
+        print()
         print("1. Generate summary of the course")
         print("2. Show the grades of all students")
         print("3. Search student's record")
